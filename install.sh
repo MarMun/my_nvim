3c75450957b6b4fba8e7b7ad97a3f7960baf8146
Martin Munstermann <ludolg01@gmail.com>
2023-01-05 12:40:26 +0100

    install telescope deps

--- install.sh
+++ install.sh
@@ -1,3 +1,55 @@
 #! /bin/bash
+os=$(uname)
 
-ln -s ~/.my_nvim/src ~/.config/nvim
+function checkInstall {
+  package=$1
+  command=$2
+
+  echo -n "check: $command"
+
+  if [ -x "$(command -v $command)" ]; then
+    echo " - installed"
+    return
+  fi
+
+  echo " - installing..."
+
+  if [ "$os" = "Linux" ]; then
+    sudo apt-get install $package
+  elif [ "$os" = "Darwin" ]; then
+    brew install $package
+  fi
+}
+
+link_target=~/.config/nvim
+
+if ! [ -e "$link_target" ]; then
+  echo "create softlink from repo to config location"
+  echo "(it's $link_target)"
+  ln -s ~/.my_nvim/src $link_target
+else
+  echo "softlink from repo to config found"
+  echo "(at $link_target)"
+fi
+
+echo "check / install dependencies"
+
+if [ "$os" = "Linux" ]; then
+
+  checkInstall "riprep" "rg"
+  checkInstall "fd-find" "fdfind"
+
+elif [ "$os" = "Darwin" ]; then
+  
+  if ! [ -x "$(command -v brew)" ]; then
+    echo "brew missing and needed"
+    return
+  fi
+  checkInstall "riprep" "rg"
+  checkInstall "fd" "fd"
+
+fi
+
+echo "we good"
+echo "start nvim and run :PackerSync"
+echo "then run :checkhealth for further things to do"
