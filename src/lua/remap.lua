20eaa1bf9a7bb9a73156d29dcc5f6f3650b42052
Martin Munstermann <martin@munstermann.io>
2023-06-17 17:07:26 +0200

    update mappings

--- src/lua/remap.lua
+++ src/lua/remap.lua
@@ -2,14 +2,16 @@ vim.g.mapleader = ","
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>e", vim.cmd.q)
+vim.keymap.set("n", "<leader>we", ':wq<CR>')
 
 vim.keymap.set("i", "jj", "<ESC>")
 vim.keymap.set("n", "//", vim.cmd.nohlsearch)
 
-vim.keymap.set("n", "<C-h>", "<C-W>h")
-vim.keymap.set("n", "<C-j>", "<C-W>j")
-vim.keymap.set("n", "<C-k>", "<C-W>k")
-vim.keymap.set("n", "<C-l>", "<C-W>l")
+vim.keymap.set("n", "<S-h>", "<C-W>h")
+vim.keymap.set("n", "<S-j>", "<C-W>j")
+vim.keymap.set("n", "<S-k>", "<C-W>k")
+vim.keymap.set("n", "<S-l>", "<C-W>l")
 
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
