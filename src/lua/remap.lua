5b766f6ea4884d909636cc694e2397deace34619
Martin Munstermann <martin@munstermann.io>
2023-06-19 15:49:17 +0200

    remap navigation

--- src/lua/remap.lua
+++ src/lua/remap.lua
@@ -8,10 +8,10 @@ vim.keymap.set("n", "<leader>we", ':wq<CR>')
 vim.keymap.set("i", "jj", "<ESC>")
 vim.keymap.set("n", "//", vim.cmd.nohlsearch)
 
-vim.keymap.set("n", "<S-h>", "<C-W>h")
-vim.keymap.set("n", "<S-j>", "<C-W>j")
-vim.keymap.set("n", "<S-k>", "<C-W>k")
-vim.keymap.set("n", "<S-l>", "<C-W>l")
+vim.keymap.set("n", "<C-h>", "<C-W>h")
+vim.keymap.set("n", "<C-j>", "<C-W>j")
+vim.keymap.set("n", "<C-k>", "<C-W>k")
+vim.keymap.set("n", "<C-l>", "<C-W>l")
 
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
