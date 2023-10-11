f94389c04f83ea90e02268d24419e08837c1f71c
Martin Munstermann <martin@munstermann.io>
2023-10-11 15:34:58 +0200

    Fix: config error

--- src/after/plugin/lsp.lua
+++ src/after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'eslint', 'sumneko_lua'},
+  ensure_installed = {'eslint'},
   handlers = {
     lsp_zero.default_setup,
   },
