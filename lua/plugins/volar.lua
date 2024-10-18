065f4d614a080e16e0d96bb40dcec24f9a23efd7
Martin Munstermann <martin@munstermann.io>
2024-10-18 09:47:29 +0200

    fix: auto format on save

--- lua/plugins/volar.lua
+++ lua/plugins/volar.lua
@@ -2,11 +2,27 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      -- Volar setup for Vue files only
       require("lspconfig").volar.setup({
-        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        filetypes = { "vue" }, -- Restrict to Vue files only
       })
+
+      -- ESLint setup with auto-fix on save
       require("lspconfig").eslint.setup({
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" }, -- Add Vue as well
+        on_attach = function(client, bufnr)
+          if client.server_capabilities.codeActionProvider then
+            -- Create a group to manage ESLint autocmds
+            local group = vim.api.nvim_create_augroup("LspESLintAutoFix", { clear = true })
+
+            -- Auto-fix ESLint issues on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = group,
+              buffer = bufnr,
+              command = "EslintFixAll", -- Run ESLint's fix command on save
+            })
+          end
+        end,
       })
     end,
   },
