a998ec50feb92c9b645b3adfcbd4068e1e502a08
Martin Munstermann <martin@munstermann.io>
2024-10-18 09:37:15 +0200

    fix: eslint / volar clash

--- lua/plugins/volar.lua
+++ lua/plugins/volar.lua
@@ -2,7 +2,12 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("lspconfig").volar.setup({})
+      require("lspconfig").volar.setup({
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+      })
+      require("lspconfig").eslint.setup({
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+      })
     end,
   },
 }
