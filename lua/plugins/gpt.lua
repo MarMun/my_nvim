10601f3995d431cd82fe9d9d8e8c445c56854f2d
Martin Munstermann <martin@munstermann.io>
2025-05-07 13:10:21 +0200

    read api keys from env

--- lua/plugins/gpt.lua
+++ lua/plugins/gpt.lua
@@ -2,7 +2,8 @@ return {
   "robitx/gp.nvim",
   config = function()
     local conf = {
-      -- openai_api_key = os.getenv("OPENAI_API_KEY"),
+      openai_api_key = os.getenv("OPENAI_API_KEY"),
+      --
       -- openai_api_key = "YOUR_API_KEY",
 
       -- For customization, refer to Install > Configuration in the Documentation/Readme
