3f42567e26077d4f9fd874249a3fb4043ac15ec6
Martin Munstermann <martin@munstermann.io>
2025-10-22 17:22:16 +0200

    chore: update gp.nvim setup / connect openrouter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,12 +18,17 @@ vim.keymap.set("n", "<leader>td", vim.cmd.tabclose, { desc = "Tab close" })
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree toggle" })
 
 -- keybinds for ai interaction
-vim.keymap.set({ "n", "v" }, "<leader>cr", ":'<,'>GpRewrite<CR>", { desc = "AI Rewrite" })
-vim.keymap.set({ "n", "v" }, "<leader>ca", ":'<,'>GpAppend<CR>", { desc = "AI Rewrite" })
-vim.keymap.set({ "n", "v" }, "<leader>ci", ":'<,'>GpImplement<CR>", { desc = "AI Implement" })
-vim.keymap.set({ "n", "v" }, "<leader>cp", ":'<,'>GpPopup<CR>", { desc = "AI Popup" })
-vim.keymap.set({ "n", "v" }, "<leader>cc", ":GpContext<CR>", { desc = "AI Context" })
-vim.keymap.set({ "n", "v" }, "<leader>cs", ":GpContext<CR>", { desc = "AI Stop" })
+vim.keymap.set({ "n", "v" }, "<leader>cvn", "<Cmd>GpChatNew Coding vsplit<CR>", { desc = "AI Chat Vertical" })
+vim.keymap.set({ "n", "v" }, "<leader>cvt", "<Cmd>GpChatToggle Coding vsplit<CR>", { desc = "AI Chat Vertical" })
+vim.keymap.set({ "n", "v" }, "<leader>cvr", "<Cmd>GpChatRespond 3<CR>", { desc = "AI Chat Respond" })
+vim.keymap.set({ "n", "v" }, "<leader>cvd", "<Cmd>GpChatDelete<CR>", { desc = "AI Chat Delete" })
+
+vim.keymap.set({ "n", "v" }, "<leader>cr", "<Cmd>GpRewrite Coding<CR>", { desc = "AI Rewrite" })
+vim.keymap.set({ "n", "v" }, "<leader>ca", "<Cmd>GpAppend Coding<CR>", { desc = "AI Append" })
+vim.keymap.set({ "n", "v" }, "<leader>ci", "<Cmd>GpImplement Coding<CR>", { desc = "AI Implement" })
+vim.keymap.set({ "n", "v" }, "<leader>cp", "<Cmd>GpPopup Chat<CR>", { desc = "AI Popup" })
+vim.keymap.set({ "n", "v" }, "<leader>cc", "<Cmd>GpContext<CR>", { desc = "AI Context" })
+vim.keymap.set({ "n", "v" }, "<leader>cs", "<Cmd>GpContext<CR>", { desc = "AI Stop" })
 
 -- Ollama - Prompt menu
 vim.keymap.set({ "n", "v" }, "<leader>oo", ":<c-u>lua require('ollama').prompt()<cr>", { desc = "Ollama Prompt" })
--- lua/plugins/gpt.lua
+++ lua/plugins/gpt.lua
@@ -2,12 +2,37 @@ return {
   "robitx/gp.nvim",
   config = function()
     local conf = {
-      openai_api_key = os.getenv("OPENAI_API_KEY"),
-      --
-      -- openai_api_key = "YOUR_API_KEY",
+      providers = {
+        openrouter = {
+          disable = false,
+          endpoint = os.getenv("OPENROUTER_API_BASE"),
+          secret = os.getenv("OPENROUTER_API_KEY"),
+        },
+      },
+      agents = {
+        {
+          name = "Chat",
+          provider = "openrouter",
+          chat = true,
+          command = true,
+          model = { model = "openai/chatgpt-4o-latest" },
+          system_prompt = "You explain and theorise about coding related topics",
+        },
+        {
+          name = "Coding",
+          provider = "openrouter",
+          chat = true,
+          command = true,
+          model = { model = "openai/gpt-5-codex" },
+          system_prompt = "You are an coding assistant focused on code completion and comment to code transformation",
+        },
+      },
 
+      default_chat_agent = "Chat",
+      --
       -- For customization, refer to Install > Configuration in the Documentation/Readme
     }
+
     require("gp").setup(conf)
 
     -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
