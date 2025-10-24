734260851e457782cb97d5d9a84cbf286345523b
Martin Munstermann <martin@munstermann.io>
2025-10-24 18:20:28 +0200

    fix: unwanted prompts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,15 +18,15 @@ vim.keymap.set("n", "<leader>td", vim.cmd.tabclose, { desc = "Tab close" })
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree toggle" })
 
 -- keybinds for ai interaction
-vim.keymap.set({ "n", "v" }, "<leader>cvn", "<Cmd>GpChatNew Coding vsplit<CR>", { desc = "AI Chat Vertical" })
-vim.keymap.set({ "n", "v" }, "<leader>cvt", "<Cmd>GpChatToggle Coding vsplit<CR>", { desc = "AI Chat Vertical" })
+vim.keymap.set({ "n", "v" }, "<leader>cvn", "<Cmd>GpChatNew vsplit<CR>", { desc = "AI Chat Vertical" })
+vim.keymap.set({ "n", "v" }, "<leader>cvt", "<Cmd>GpChatToggle vsplit<CR>", { desc = "AI Chat Vertical" })
 vim.keymap.set({ "n", "v" }, "<leader>cvr", "<Cmd>GpChatRespond 3<CR>", { desc = "AI Chat Respond" })
 vim.keymap.set({ "n", "v" }, "<leader>cvd", "<Cmd>GpChatDelete<CR>", { desc = "AI Chat Delete" })
 
-vim.keymap.set({ "n", "v" }, "<leader>cr", "<Cmd>GpRewrite Coding<CR>", { desc = "AI Rewrite" })
-vim.keymap.set({ "n", "v" }, "<leader>ca", "<Cmd>GpAppend Coding<CR>", { desc = "AI Append" })
-vim.keymap.set({ "n", "v" }, "<leader>ci", "<Cmd>GpImplement Coding<CR>", { desc = "AI Implement" })
-vim.keymap.set({ "n", "v" }, "<leader>cp", "<Cmd>GpPopup Chat<CR>", { desc = "AI Popup" })
+vim.keymap.set({ "n", "v" }, "<leader>cr", "<Cmd>GpRewrite<CR>", { desc = "AI Rewrite" })
+vim.keymap.set({ "n", "v" }, "<leader>ca", "<Cmd>GpAppend<CR>", { desc = "AI Append" })
+vim.keymap.set({ "n", "v" }, "<leader>ci", "<Cmd>GpImplement <CR>", { desc = "AI Implement" })
+vim.keymap.set({ "n", "v" }, "<leader>cp", "<Cmd>GpPopup<CR>", { desc = "AI Popup" })
 vim.keymap.set({ "n", "v" }, "<leader>cc", "<Cmd>GpContext<CR>", { desc = "AI Context" })
 vim.keymap.set({ "n", "v" }, "<leader>cs", "<Cmd>GpContext<CR>", { desc = "AI Stop" })
 
