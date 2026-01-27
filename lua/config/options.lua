36b57da4ee442cdf098f97c12b565a16b794b70f
Martin Munstermann <martin@munstermann.io>
2026-01-27 18:10:05 +0100

    fix: kotlin lsp using wrong jdk (25 instead of 21)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local jdk = vim.fn.system("brew --prefix openjdk@21"):gsub("%s+$", "")
+vim.env.JAVA_HOME = jdk
+vim.env.PATH = jdk .. "/bin:" .. vim.env.PATH
