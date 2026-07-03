ad33e8280b80035cf4616f0c79730c229e3e4860
Martin Munstermann <martin@munstermann.io>
2026-07-03 16:56:29 +0200

    Add dadbod plugin

---
+++ lua/plugins/dadbod.lua
@@ -0,0 +1,18 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/db_ui"
+  end,
+}
